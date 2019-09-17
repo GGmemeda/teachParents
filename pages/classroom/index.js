3121fd4b22069f4066d8afe9331202a4ca34e708
李永辉 <2395095699@qq.com>
2019-09-17 16:22:50 +0800

    首页，成绩

---
+++ pages/classroom/index.js
@@ -0,0 +1,81 @@
+// pages/classroom/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  showAction() {
+    wx.showActionSheet({
+      itemList: ['切换课堂', '新增课堂', '修改课堂名称', '删除课堂'],
+      success: function (e) {
+        console.log(e.tapIndex) //没有item项下的key或index
+      }
+    })
+  },
+
+  actionChange(i) {
+    if (!i) {
+
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/results/index.js
@@ -0,0 +1,74 @@
+// pages/results/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabArr: ["独立考试(30)", "统一考试(30)"],
+    navTabIndex: 0
+  },
+
+  pageTabFun(e) {
+    this.setData({
+      navTabIndex: e.detail.index
+    })
+    console.log(e.detail.index)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
